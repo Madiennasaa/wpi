75934dd5adc13fe54080c54d73e8e36cb9b6fb82
ropip12 <rofiiffadhlurroman@gmail.com>
2026-01-29 10:38:15 +0700

    coba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -892,7 +892,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M17 8l4 4m0 0l-4 4m4-4H3"/>
                 </svg>
             </a>
-            <a href="#" class="btn-secondary inline-flex items-center justify-center px-8 py-4 rounded-full text-base font-bold">
+            <a href="/kontak" class="btn-secondary inline-flex items-center justify-center px-8 py-4 rounded-full text-base font-bold">
                 HUBUNGI KAMI
             </a>
         </div>
